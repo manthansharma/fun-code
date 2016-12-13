6a907223036c045c6ba2dbd16e0b5d14f888c440
Manthan Sharma <hitechmanthan@gmail.com>
2016-12-13 15:29:55 +0530

    Update Python code for PEP8 compliance

--- python/whatsapp-bot.py
+++ python/whatsapp-bot.py
@@ -24,46 +24,46 @@ msg_arr = []  # Define all message in array format
 group = True  # Define above contact array is group or not
 
 try:
-	driver.get('http://web.whatsapp.com')
-	input("Press c when load complete :> ")  # Wait program to proceed till load complete
+    driver.get('http://web.whatsapp.com')
+    input("Press c when load complete :> ")  # Wait program to proceed till load complete
 
-	for name in name_arr:
-		if not group:
-			# Click on contact button if group set to false
-			contact_btn = driver.find_element_by_class_name('icon-chat')
-			contact_btn.click()
+    for name in name_arr:
+        if not group:
+            # Click on contact button if group set to false
+            contact_btn = driver.find_element_by_class_name('icon-chat')
+            contact_btn.click()
 
-		# Fill the Search Input with the contact name
-		time.sleep(1)
-		search_input = driver.find_element_by_class_name('input-search')
-		search_input.send_keys(name)
+        # Fill the Search Input with the contact name
+        time.sleep(1)
+        search_input = driver.find_element_by_class_name('input-search')
+        search_input.send_keys(name)
 
-		# Click on contact to open chat
-		time.sleep(1)
-		contact = driver.find_element_by_xpath('//span[contains(text(),"' + name + '")]')
-		contact.click()
+        # Click on contact to open chat
+        time.sleep(1)
+        contact = driver.find_element_by_xpath('//span[contains(text(),"' + name + '")]')
+        contact.click()
 
-		for msg in msg_arr:
-			msg_input = driver.find_element_by_xpath("//div[@data-tab='1'][@spellcheck='true'][@class='input']")
-			for msg_elem in msg.split('\n'):
-				# Write Message in chat Box
-				msg_input.send_keys(msg_elem)
-				# Shift + Enter for new line
-				ActionChains(driver).move_to_element(msg_input) \
-					.key_down(Keys.SHIFT) \
-					.key_down(Keys.ENTER) \
-					.key_up(Keys.ENTER) \
-					.key_up(Keys.SHIFT) \
-					.perform()
-			msg_input.send_keys("_-Send by Python Bot!! Hail J.A.R.V.I.S.")
-			driver.find_element_by_class_name('send-container').click()  # Click on send Button
+        for msg in msg_arr:
+            msg_input = driver.find_element_by_xpath("//div[@data-tab='1'][@spellcheck='true'][@class='input']")
+            for msg_elem in msg.split('\n'):
+                # Write Message in chat Box
+                msg_input.send_keys(msg_elem)
+                # Shift + Enter for new line
+                ActionChains(driver).move_to_element(msg_input) \
+                    .key_down(Keys.SHIFT) \
+                    .key_down(Keys.ENTER) \
+                    .key_up(Keys.ENTER) \
+                    .key_up(Keys.SHIFT) \
+                    .perform()
+            msg_input.send_keys("_-Send by Python Bot!! Hail J.A.R.V.I.S.")
+            driver.find_element_by_class_name('send-container').click()  # Click on send Button
 
-			print("Message sent to Name :> " + name + ", Message:> " + msg)
-		time.sleep(10)
-		name_arr_rev.pop()
+            print("Message sent to Name :> " + name + ", Message:> " + msg)
+        time.sleep(10)
+        name_arr_rev.pop()
 except Exception as e:
-	print(e)
-	print("Remaining Name :> " + name_arr_rev)
+    print(e)
+    print("Remaining Name :> " + name_arr_rev)
 finally:
-	input("Exit> ")
-	driver.quit()
+    input("Exit> ")
+    driver.quit()
